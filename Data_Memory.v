033836b07aaaa50b0a13bb7d4ee876b4e5401a16
arghyoo <arghyar004@gmail.com>
2024-12-24 13:22:52 +0530

    Add files via upload

---
+++ Data_Memory.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/03/2024 02:04:24 AM
+// Design Name: 
+// Module Name: Data_Memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Data_Memory(clk,rst,WE,WD,A,RD);
+
+    input clk,rst,WE;
+    input [31:0]A,WD;
+    output [31:0]RD;
+
+    reg [31:0] mem [1023:0];
+
+    always @ (posedge clk)
+    begin
+        if(WE)
+            mem[A] <= WD;
+    end
+
+    assign RD = (~rst) ? 32'd0 : mem[A];
+
+    initial begin
+        mem[28] = 32'h00000020;
+        //mem[40] = 32'h00000002;
+    end
+
+
+endmodule
